9e4f794385a86e4f62a4b56f641bd522144ac82c
piyushd3112 <dhonglep@gmail.com>
2024-11-15 18:00:27 +0530

    completed my services section

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,34 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "01",
+        s_name: "Frontend Development",
+        s_desc: "Building dynamic, user-friendly web applications using React.js, HTML, CSS, JavaScript, Bootstrap, and Tailwind CSS."
     },
     {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "02",
+        s_name: "UI/UX Design Integration",
+        s_desc: "Collaborating with designers to implement intuitive and responsive user interfaces that enhance the overall user experience."
     },
     {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "03",
+        s_name: "Web Application Development",
+        s_desc: "Developing full-stack web applications using React.js for the front end and Node.js/Express.js for the backend, ensuring seamless functionality."
     },
     {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "04",
+        s_name: "Bug Fixing and Optimization",
+        s_desc: "Identifying and resolving bugs, optimizing performance to deliver smooth, fast, and reliable user experiences."
     },
     {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "05",
+        s_name: "Digital Marketing",
+        s_desc: "Implementing strategies to increase online visibility, engagement, and conversion rates through SEO, social media, and online campaigns."
     },
     {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-]
- 
+        s_no: "06",
+        s_name: "SEO Optimization",
+        s_desc: "Optimizing websites for search engines to increase organic traffic, improve search rankings, and enhance user visibility."
+    }
+];
+
 export default Services_Data;
